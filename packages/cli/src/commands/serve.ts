4a25b0a5d13743b4e301e4e875ed65ef15bdc6e2
Mark Johnson <marklj@gmail.com>
2022-03-28 09:58:24 -0700

    add error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -6,7 +6,22 @@ export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(Number(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(Number(options.port), path.basename(filename), dir);
+      console.log(`Opened ${filename} at http://localhost:${options.port}`);
+    } catch (err) {
+      if (err.code === "EADDRINUSE") {
+        console.error(
+          "Port already in use. Try running on a different port by using --port=PORT-NUMBER."
+        );
+      } else {
+        console.error(
+          "Yikes! There was a problem starting the server!",
+          err.message
+        );
+      }
+      process.exit(1);
+    }
   });
