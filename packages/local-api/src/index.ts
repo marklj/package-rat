b5d2980f12ad913ad60f1d150e1176716b9ec37e
Mark Johnson <marklj@gmail.com>
2022-03-27 19:05:13 -0700

    implement serve command

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log("server is listening");
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log("serving traffic on port", port);
+  console.log("file", filename);
+  console.log("dir", dir);
 };
