f682230a6dda1325c5b50862d57d2c1009d63387
Mark Johnson <marklj@gmail.com>
2022-03-31 07:29:56 -0700

    prep packages for publishing to npm

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
-import { serve } from "local-api";
+import { serve } from "@pkgrat/local-api";
 import path from "path";
 
 const isProduction = process.env.NODE_ENV === "production";
@@ -8,7 +8,7 @@ export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action(async (filename = "notebook.js", options: { port: string }) => {
+  .action(async (filename = "pack.js", options: { port: string }) => {
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
       await serve(
